85ccd0e68baf4d2c47468ee98aee133acfd54eb6
unknown <craftopiami5@gmail.com>
2026-07-24 12:22:30 -0700

    Update Supabase config and deploy credentials

--- update_webhook.js
+++ update_webhook.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
-const oldToken = '8864741035:AAF5BMri8NIWEhJfwUq7DGmkiwQ86zB5o8o';
-const newToken = '8602881468:AAF04TGYwH18uuKPlGhC3qtAnmFBfHrZh_4';
+const botToken = '8906068445:AAGc5L08H9a1Lc0oYIDL9o4ZqjJbLVMII4Y';
+const edgeFunctionUrl = 'https://acnaidlegwkqcjxbdwra.supabase.co/functions/v1/api';
 
 function request(url) {
     return new Promise((resolve, reject) => {
@@ -14,15 +14,9 @@ function request(url) {
 }
 
 async function run() {
-    console.log("Deleting old webhook...");
-    const oldRes = await request(`https://api.telegram.org/bot${oldToken}/deleteWebhook`);
-    console.log(oldRes);
-    
-    console.log("Deleting new webhook (to enable local polling)...");
-    const newRes = await request(`https://api.telegram.org/bot${newToken}/deleteWebhook`);
-    console.log(newRes);
-    
-    console.log("Done. If you are hosting on Vercel, you need to hit /api/bot/setup on your deployed URL to set it.");
+    console.log(`Setting webhook to ${edgeFunctionUrl}...`);
+    const res = await request(`https://api.telegram.org/bot${botToken}/setWebhook?url=${encodeURIComponent(edgeFunctionUrl)}`);
+    console.log("Telegram API Response:", res);
 }
 
 run();
